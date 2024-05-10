68d7019b4071c8610a9e2bc17fb7ac8af6158175
lancelot26 <kirua.fil@gmail.com>
2024-05-10 12:46:57 +0700

    Individual diary for each user

--- diary/diary/settings.py
+++ diary/diary/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'diary.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'lancelot',
+        'USER': 'postgres',
+        'PASSWORD': 'pass_db', # I changed it
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
